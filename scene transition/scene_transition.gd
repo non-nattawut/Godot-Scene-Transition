be2c19ee9287ea488e8cc55a478383d6a609ab81
Nattawut Rodthong <nattavutnon@gmail.com>
2024-08-21 14:12:06 +0700

    Add files via upload

--- scene transition/scene_transition.gd
+++ scene transition/scene_transition.gd
@@ -7,6 +7,3 @@ func change_scene(target : String) -> void:
 	await animation_player.animation_finished
 	get_tree().change_scene_to_file(target)
 	animation_player.play_backwards("dissolve")
-	
-	GlobalVar.pause(false) # handle ในกรณีของ popup มัน pause game ไว้ก็ให้ unpause
-	

00b5a137c221d28c4bb0c4f3deaf594a8af47e91
Nattawut Rodthong <nattavutnon@gmail.com>
2024-08-21 14:05:44 +0700

    Add files via upload

---
+++ scene transition/scene_transition.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+@onready var animation_player = $AnimationPlayer
+
+func change_scene(target : String) -> void:
+	animation_player.play("dissolve")
+	await animation_player.animation_finished
+	get_tree().change_scene_to_file(target)
+	animation_player.play_backwards("dissolve")
+	
+	GlobalVar.pause(false) # handle ในกรณีของ popup มัน pause game ไว้ก็ให้ unpause
+	
